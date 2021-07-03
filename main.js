82f6fb592639c31942daa9e00879bd1aff04575a
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-07-03 11:37:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,3 +30,54 @@ function take_snapshot()
         }
     
 
+function check()
+        {
+            img = document.getElementById('captured_image');
+            classifier.classify(img, gotResult);
+        }
+        function speak()
+        {
+            var synth = window.speechSynthesis;
+            speak_data_1 = "The first prediction is " + prediction_1;
+            speak_data_2 = "The second prediction is " + prediction_2;
+            var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+            synth.speak(utterThis);
+        }
+        function gotResult(error, results) {
+            if(error) {
+            console.error(error);
+            }else {
+            console.log(results);
+            document.getElementById("result_gesture_name").innerHTML = results[0].label;
+            document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+            prediction_1 = results[0].label;
+            prediction_2 = results[1].label;
+            speak();
+            if(results[0].label == "OK gesture")
+            {
+                document.getElementById("update_gesture").innerHTML = "&#128076;";
+            }
+            if(results[0].label == "Thumbs up gesture")
+            {
+                document.getElementById("update_gesture").innerHTML = "&#128077;";
+            }
+            if(results[0].label == "Stop gesture")
+            {
+                document.getElementById("update_gesture").innerHTML = "&#9995;";
+            }
+
+            if(results[1].label == "OK gesture")
+            {
+                document.getElementById("update_gesture2").innerHTML = "&#128076;";
+            }
+            if(results[1].label == "Thumbs up gesture")
+            {
+                document.getElementById("update_gesture2").innerHTML = "&#128077;";
+            }
+            if(results[1].label == "Stop gesture")
+            {
+                document.getElementById("update_gesture2").innerHTML = "&#9995;";
+            }
+        }
+            
+    }
