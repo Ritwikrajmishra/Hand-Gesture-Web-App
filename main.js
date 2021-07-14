f5e98cdeed9dde228de88c99827f0fcf0c485b5d
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-07-14 11:13:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function take_snapshot()
     });
 }
         console.log('ml5 version:', ml5.version);
-        classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/HFFtyEJcp/', modelLoaded)
+        classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/HFFtyEJcp/model.json', modelLoaded)
         function modelLoaded()
         {
             console.log('Model Loaded!');
